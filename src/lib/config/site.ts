342893adb3efda03594b88442bffaba7d785c6ba
Andrew Nash <anash@mun.ca>
2023-06-16 12:10:52 -0230

    update domain to fix favicon

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'andrewnash.github.io',
   title: 'Andrew Nash Blog',
   subtitle: 'MSc',
   lang: 'en-US',
